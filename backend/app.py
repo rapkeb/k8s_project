5377d58f4b93549d10e70b3c27190938f219b070
rapkeb <rapkeb@post.bgu.ac.il>
2024-07-14 14:56:35 +0300

    add service for mongo and connect it to the backend

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,25 @@
 from flask import Flask, jsonify
+import pandas as pd
 from pymongo import MongoClient
 
 
 app = Flask(__name__)
 
-# Connect to MongoDB
-client = MongoClient('mongodb://localhost:27017/')  # Replace with your MongoDB URI
-db = client['churn_prediction']  # Replace with your database name
-collection = db['raw_data']  # Replace with your collection name
+try:
+    # Adjust the connection string as per your Kubernetes setup
+    client = MongoClient('mongo-service:27017')
+    # List all databases
+    print(f"Available databases: {client.list_database_names()}")
+    # Use a specific database
+    db = client.churn_prediction
+    # List collections in the database
+    print(f"Collections in churn_prediction: {db.list_collection_names()}")
+    # Use a specific collection
+    collection = db.raw_data
+    # Example query
+    print(f"First document in raw_data: {collection.find_one()}")
+except Exception as e:
+    print(f"Error connecting to MongoDB: {e}")
 
 
 @app.route('/submit/<user_id>', methods=['GET'])
@@ -20,6 +32,20 @@ def submit(user_id):
         return jsonify(user)
     else:
         return jsonify({"error": f"User ID {user_id} not found"}), 404
+
+    
+@app.route('/upload', methods=['POST'])
+def upload_csv():
+    df = pd.read_csv('churn.csv')
+    print(df.head)
+    data = df.to_dict(orient='records')
+    result = collection.insert_many(data)
+    return jsonify({'result': 'Data inserted successfully', 'inserted_ids': str(result.inserted_ids)}), 201
+    
+
+@app.route('/flask/kinan', methods=['Get'])
+def get_kinan():
+    return f'The current time is: kinan'
 
 
 if __name__ == '__main__':
